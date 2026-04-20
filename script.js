921adc65852850daa9b66448e380c0e971478f3d
kyz blop <jeremymoi.jd@gmail.com>
2026-04-20 16:56:53 +0200

    Footer

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ fetch("header.html")
     document.querySelector("#header").innerHTML = data;
 });
 
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.querySelector("#footer").innerHTML = data;
+});
+
