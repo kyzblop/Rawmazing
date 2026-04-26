e2bb833a0e24fbf971e55f65a8d6e4b77447a19e
kyz blop <jeremymoi.jd@gmail.com>
2026-04-27 00:01:44 +0200

    Page d'accueil avec responsive

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 fetch("header.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.querySelector("#header").innerHTML = data;
-});
+
+    const burger = document.querySelector(".burger_btn");
+    const nav = document.querySelector(".nav-links");
+
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  });
 
 fetch("footer.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.querySelector("#footer").innerHTML = data;
-});
-
+  });
