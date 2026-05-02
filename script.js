44d3141e72cce96764514914852a3855a6259423
kyz blop <jeremymoi.jd@gmail.com>
2026-05-02 12:39:54 +0200

    Ajout maintenance et 404, actualités en cours

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let index = 0;
+let actualites = [];
+
 fetch("header.html")
   .then((res) => res.text())
   .then((data) => {
@@ -16,3 +19,34 @@ fetch("footer.html")
   .then((data) => {
     document.querySelector("#footer").innerHTML = data;
   });
+
+
+fetch("actualites.json")
+  .then((res) => res.json())
+  .then((data) => {
+    actualites = data;
+    afficherActualite(index);
+  });
+
+function afficherActualite(i) {
+  document.querySelector('.actualite-date').textContent = actualites[i].date;
+  document.querySelector('.actualite-message').textContent = actualites[i].message;
+  
+  const img = document.querySelector('.actualite-image');
+  if (actualites[i].image) {
+    img.src = actualites[i].image;
+    img.style.display = 'block';
+  } else {
+    img.style.display = 'none';
+  }
+}
+
+document.querySelector(".prev").addEventListener("click", () => {
+  index = (index - 1 + actualites.length) % actualites.length;
+  afficherActualite(index);
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  index = (index + 1) % actualites.length;
+  afficherActualite(index);
+});
